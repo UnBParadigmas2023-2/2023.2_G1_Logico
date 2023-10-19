3e0b5e6dded4dc049b2802332d94dbb5d6191284
Rodrigo Santos <ro12062000@gmail.com>
2023-10-19 12:33:00 -0300

    :card_file_box: add data disciplinas obrigatórias
    Foi adicionado a base de dados de disciplinas contendo Código,
    Quantidade Credito, se é Obriagatória e seu Nome.

---
+++ src/database/disciplinas_obrigatorias.pl
@@ -0,0 +1,63 @@
+% disciplina(Codigo, QtdCredito, Obrigatorio, Nome).
+% 1º Semestre
+disciplina("MAT0025", 6, true, "CÁLCULO 1").
+disciplina("CIC0004", 6, true, "ALGORITMOS E PROGRAMAÇÃO DE COMPUTADORES").
+disciplina("FGA0168", 6, true, "DESENHO INDUSTRIAL ASSISTIDO POR COMPUTADOR").
+disciplina("FGA0161", 4, true, "ENGENHARIA E AMBIENTE").
+disciplina("FGA0163", 4, true, "INTRODUÇÃO À ENGENHARIA").
+
+% 2º Semestre
+disciplina("MAT0026", 6, true, "CÁLCULO 2").
+disciplina("IFD0171", 4, true, "FISICA 1").
+disciplina("IFD0173", 2, true, "FISICA 1 EXPERIMENTAL").
+disciplina("MAT0031", 4, true, "INTRODUCAO A ALGEBRA LINEAR").
+disciplina("FGA0157", 4, true, "PROBABILIDADE E ESTATÍSTICA APLICADO A ENGENHARIA").
+
+% 3º Semestre
+disciplina("FGA0160", 4, true, "MÉTODOS NUMÉRICOS PARA ENGENHARIA").
+disciplina("FGA0133", 4, true, "ENGENHARIA ECONÔMICA").
+disciplina("FGA0164", 4, true, "HUMANIDADES E CIDADANIA").
+disciplina("FGA0073", 4, true, "TEORIA DE ELETRÔNICA DIGITAL 1").
+disciplina("FGA0071", 2, true, "PRÁTICA DE ELETRÔNICA DIGITAL 1").
+disciplina("FGA0158", 4, true, "ORIENTAÇÃO A OBJETOS").
+disciplina("FGA0085", 4, true, "MATEMÁTICA DISCRETA 1").
+
+% 4º Semestre
+disciplina("FGA0184", 4, true, "GESTÃO DA PRODUÇÃO E QUALIDADE").
+disciplina("FGA0138", 4, true, "MÉTODOS DE DESENVOLVIMENTO DE SOFTWARE").
+disciplina("FGA0147", 4, true, "ESTRUTURA DE DADOS E ALGORITMOS").
+disciplina("FGA0142", 4, true, "FUNDAMENTOS DE ARQUITETURA DE COMPUTADORES").
+disciplina("FGA0108", 4, true, "MATEMÁTICA DISCRETA 2").
+disciplina("FGA0150", 4, true, "PROJETO INTEGRADOR DE ENGENHARIA 1").
+
+% 5º Semestre
+disciplina("FGA0173", 4, true, "INTERAÇÃO HUMANO COMPUTADOR").
+disciplina("FGA0172", 4, true, "REQUISITOS DE SOFTWARE").
+disciplina("FGA0137", 4, true, "SISTEMAS DE BANCO DE DADOS 1").
+disciplina("FGA0170", 4, true, "FUNDAMENTOS DE SISTEMAS OPERACIONAIS").
+disciplina("FGA0003", 4, true, "COMPILADORES 1").
+disciplina("FGA0030", 4, true, "ESTRUTURAS DE DADOS 2").
+
+% 6º Semestre
+disciplina("FGA0278", 4, true, "QUALIDADE DE SOFTWARE 1").
+disciplina("FGA0238", 4, true, "TESTES DE SOFTWARE").
+disciplina("FGA0208", 4, true, "ARQUITETURA E DESENHO DE SOFTWARE").
+disciplina("FGA0211", 4, true, "FUNDAMENTOS DE REDES DE COMPUTADORES").
+disciplina("FGA0060", 4, true, "SISTEMAS DE BANCO DE DADOS 2").
+
+% 7º Semestre
+disciplina("FGA0242", 4, true, "TÉCNICAS DE PROGRAMAÇÃO EM PLATAFORMAS EMERGENTES").
+disciplina("FGA0210", 4, true, "PARADIGMAS DE PROGRAMAÇÃO").
+disciplina("FGA0109", 4, true, "FUNDAMENTOS DE SISTEMAS EMBARCADOS").
+disciplina("FGA0244", 4, true, "PROGRAMAÇÃO PARA SISTEMAS PARALELOS E DISTRIBUÍDOS").
+
+% 8º Semestre
+disciplina("FGA0206", 4, true, "ENGENHARIA DE PRODUTO DE SOFTWARE").
+disciplina("FGA0240", 4, true, "GERÊNCIA DE CONFIGURAÇÃO E EVOLUÇÃO DE SOFTWARE").
+
+% 9º Semestre
+disciplina("FGA0250", 6, true, "PROJETO INTEGRADOR DE ENGENHARIA 2").
+disciplina("FGA0009", 4, true, "TRABALHO DE CONCLUSÃO DE CURSO 1").
+
+% 10º Semestre
+disciplina("FGA0011", 4, true, "TRABALHO DE CONCLUSÃO DE CURSO 2").
