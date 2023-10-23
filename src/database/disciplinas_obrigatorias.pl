f85191cd513cb81c2fc4578a71cdfa2deae3ed0d
Pedro Vitor Augusto de Jesus <pedrovitora.jesus@gmail.com>
2023-10-23 11:15:14 -0300

    fix: Remove caracteres especiais da base
    Ao realizar consultas foi detectado inconsistências na maneira que foi exibido os dados com carateres especias, dessa forma os mesmo foram alterados para que nao apresentassem esses erros
    https://github.com/UnBParadigmas2023-2/2023.2_G1_Logico/issues/2

--- src/database/disciplinas_obrigatorias.pl
+++ src/database/disciplinas_obrigatorias.pl
@@ -1,37 +1,37 @@
 % disciplina(Codigo, QtdCredito, Obrigatorio, Nome).
 % 1º Semestre
-disciplina("MAT0025", 6, true, "CÁLCULO 1").
-disciplina("CIC0004", 6, true, "ALGORITMOS E PROGRAMAÇÃO DE COMPUTADORES").
+disciplina("MAT0025", 6, true, "CALCULO 1").
+disciplina("CIC0004", 6, true, "ALGORITMOS E PROGRAMACAO DE COMPUTADORES").
 disciplina("FGA0168", 6, true, "DESENHO INDUSTRIAL ASSISTIDO POR COMPUTADOR").
 disciplina("FGA0161", 4, true, "ENGENHARIA E AMBIENTE").
-disciplina("FGA0163", 4, true, "INTRODUÇÃO À ENGENHARIA").
+disciplina("FGA0163", 4, true, "INTRODUCAO A ENGENHARIA").
 
 % 2º Semestre
-disciplina("MAT0026", 6, true, "CÁLCULO 2").
+disciplina("MAT0026", 6, true, "CALCULO 2").
 disciplina("IFD0171", 4, true, "FISICA 1").
 disciplina("IFD0173", 2, true, "FISICA 1 EXPERIMENTAL").
 disciplina("MAT0031", 4, true, "INTRODUCAO A ALGEBRA LINEAR").
-disciplina("FGA0157", 4, true, "PROBABILIDADE E ESTATÍSTICA APLICADO A ENGENHARIA").
+disciplina("FGA0157", 4, true, "PROBABILIDADE E ESTATISTICA APLICADO A ENGENHARIA").
 
 % 3º Semestre
-disciplina("FGA0160", 4, true, "MÉTODOS NUMÉRICOS PARA ENGENHARIA").
-disciplina("FGA0133", 4, true, "ENGENHARIA ECONÔMICA").
+disciplina("FGA0160", 4, true, "METODOS NUMERICOS PARA ENGENHARIA").
+disciplina("FGA0133", 4, true, "ENGENHARIA ECONOMICA").
 disciplina("FGA0164", 4, true, "HUMANIDADES E CIDADANIA").
-disciplina("FGA0073", 4, true, "TEORIA DE ELETRÔNICA DIGITAL 1").
-disciplina("FGA0071", 2, true, "PRÁTICA DE ELETRÔNICA DIGITAL 1").
-disciplina("FGA0158", 4, true, "ORIENTAÇÃO A OBJETOS").
-disciplina("FGA0085", 4, true, "MATEMÁTICA DISCRETA 1").
+disciplina("FGA0073", 4, true, "TEORIA DE ELETRONICA DIGITAL 1").
+disciplina("FGA0071", 2, true, "PRATICA DE ELETRONICA DIGITAL 1").
+disciplina("FGA0158", 4, true, "ORIENTACAO A OBJETOS").
+disciplina("FGA0085", 4, true, "MATEMATICA DISCRETA 1").
 
 % 4º Semestre
-disciplina("FGA0184", 4, true, "GESTÃO DA PRODUÇÃO E QUALIDADE").
-disciplina("FGA0138", 4, true, "MÉTODOS DE DESENVOLVIMENTO DE SOFTWARE").
+disciplina("FGA0184", 4, true, "GESTAO DA PRODUCAO E QUALIDADE").
+disciplina("FGA0138", 4, true, "METODOS DE DESENVOLVIMENTO DE SOFTWARE").
 disciplina("FGA0147", 4, true, "ESTRUTURA DE DADOS E ALGORITMOS").
 disciplina("FGA0142", 4, true, "FUNDAMENTOS DE ARQUITETURA DE COMPUTADORES").
-disciplina("FGA0108", 4, true, "MATEMÁTICA DISCRETA 2").
+disciplina("FGA0108", 4, true, "MATEMATICA DISCRETA 2").
 disciplina("FGA0150", 4, true, "PROJETO INTEGRADOR DE ENGENHARIA 1").
 
 % 5º Semestre
-disciplina("FGA0173", 4, true, "INTERAÇÃO HUMANO COMPUTADOR").
+disciplina("FGA0173", 4, true, "INTERACAO HUMANO COMPUTADOR").
 disciplina("FGA0172", 4, true, "REQUISITOS DE SOFTWARE").
 disciplina("FGA0137", 4, true, "SISTEMAS DE BANCO DE DADOS 1").
 disciplina("FGA0170", 4, true, "FUNDAMENTOS DE SISTEMAS OPERACIONAIS").
@@ -46,18 +46,18 @@ disciplina("FGA0211", 4, true, "FUNDAMENTOS DE REDES DE COMPUTADORES").
 disciplina("FGA0060", 4, true, "SISTEMAS DE BANCO DE DADOS 2").
 
 % 7º Semestre
-disciplina("FGA0242", 4, true, "TÉCNICAS DE PROGRAMAÇÃO EM PLATAFORMAS EMERGENTES").
-disciplina("FGA0210", 4, true, "PARADIGMAS DE PROGRAMAÇÃO").
+disciplina("FGA0242", 4, true, "TECNICAS DE PROGRAMACAO EM PLATAFORMAS EMERGENTES").
+disciplina("FGA0210", 4, true, "PARADIGMAS DE PROGRAMACAO").
 disciplina("FGA0109", 4, true, "FUNDAMENTOS DE SISTEMAS EMBARCADOS").
-disciplina("FGA0244", 4, true, "PROGRAMAÇÃO PARA SISTEMAS PARALELOS E DISTRIBUÍDOS").
+disciplina("FGA0244", 4, true, "PROGRAMACAO PARA SISTEMAS PARALELOS E DISTRIBUIDOS").
 
 % 8º Semestre
 disciplina("FGA0206", 4, true, "ENGENHARIA DE PRODUTO DE SOFTWARE").
-disciplina("FGA0240", 4, true, "GERÊNCIA DE CONFIGURAÇÃO E EVOLUÇÃO DE SOFTWARE").
+disciplina("FGA0240", 4, true, "GERÊNCIA DE CONFIGURACAO E EVOLUCAO DE SOFTWARE").
 
 % 9º Semestre
 disciplina("FGA0250", 6, true, "PROJETO INTEGRADOR DE ENGENHARIA 2").
-disciplina("FGA0009", 4, true, "TRABALHO DE CONCLUSÃO DE CURSO 1").
+disciplina("FGA0009", 4, true, "TRABALHO DE CONCLUSAO DE CURSO 1").
 
 % 10º Semestre
-disciplina("FGA0011", 4, true, "TRABALHO DE CONCLUSÃO DE CURSO 2").
+disciplina("FGA0011", 4, true, "TRABALHO DE CONCLUSAO DE CURSO 2").
