055eaab1b20a7c569842c90dc9e141f665645dec
Pedro Vitor Augusto de Jesus <pedrovitora.jesus@gmail.com>
2023-10-27 23:40:09 -0300

    fix: corrige fluxo de conflito

--- src/logic/conflito.pl
+++ src/logic/conflito.pl
@@ -1,43 +1,34 @@
 :- consult("../database/turmas.pl").
 
-% Verifica se há conflito de horário entre duas turmas
 
-conflito_de_horario(Turma1, Turma2) :-
+verificarConflitos(Turmas, TurmasSemConflito) :-
+    removerConflitos(Turmas, [], TurmasSemConflito).
+
+% Predicado para remover turmas conflitantes de uma lista de turmas
+removerConflitos([], TurmasSemConflito, TurmasSemConflito).
+removerConflitos([Turma | Resto], TurmasAtuais, TurmasSemConflito) :-
+    \+ temConflito(Turma, TurmasAtuais),
+    append(TurmasAtuais, [Turma], NovasTurmasAtuais),
+    removerConflitos(Resto, NovasTurmasAtuais, TurmasSemConflito).
+removerConflitos([_ | Resto], TurmasAtuais, TurmasSemConflito) :-
+    removerConflitos(Resto, TurmasAtuais, TurmasSemConflito).
+
+
+% Predicado para verificar se uma turma tem conflito de horário com uma lista de turmas
+temConflito(Turma, Turmas) :-
+    member(OutraTurma, Turmas),
+    Turma \== OutraTurma,
+    temChoqueHorario(Turma, OutraTurma).
+
+% Predicado para verificar se duas turmas têm choque de horário
+temChoqueHorario(Turma1, Turma2) :-
     turma(Turma1, _, Horario1),
     turma(Turma2, _, Horario2),
-    Turma1 \= Turma2,
-    tem_conflito_de_horario(Horario1, Horario2).
+    split_string(Horario1, " ", "", ListaHorario1),
+    split_string(Horario2, " ", "", ListaHorario2),
+    temIntersecao(ListaHorario1, ListaHorario2).
 
-% Verifica se há conflito de horário entre os horários
-tem_conflito_de_horario(Horario1, Horario2) :-
-    horario_dia(Horario1, Dia1),
-    horario_dia(Horario2, Dia2),
-    Dia1 = Dia2, % O conflito ocorre se os dias forem iguais
-    horario_horario(Horario1, Hora1),
-    horario_horario(Horario2, Hora2),
-    Hora1 = Hora2. % O conflito ocorre se pelo menos um horário for igual
-
-% Função para extrair o dia do horário
-horario_dia(Horario, Dia) :-
-    sub_atom(Horario, 0, 1, _, Dia).
-
-% Função para extrair o horário do dia
-horario_horario(Horario, HorarioDia) :-
-    sub_atom(Horario, 1, 1, _, HorarioDia).
-
-
-% Função para verificar se uma matéria não entra em conflito com nenhuma outra em uma lista
-nao_entra_em_conflito(_, []).
-nao_entra_em_conflito(Mat, [Mat2 | Resto]) :-
-    \+ conflito_de_horario(Mat, Mat2),
-    nao_entra_em_conflito(Mat, Resto).
-
-% Função principal para retornar matérias que não entram em conflito de horário
-materias_sem_conflito_horario([], []).
-materias_sem_conflito_horario([Mat | Resto], MatSemConflito) :-
-    nao_entra_em_conflito(Mat, Resto),
-    materias_sem_conflito_horario(Resto, MatSemConflitoResto),
-    append([Mat], MatSemConflitoResto, MatSemConflito).
-materias_sem_conflito_horario([_ | Resto], MatSemConflito) :-
-    materias_sem_conflito_horario(Resto, MatSemConflito).
-
+% Predicado para verificar se duas listas têm interseção
+temIntersecao([], _) :- false.
+temIntersecao([H|T], Lista2) :-
+    (member(H, Lista2) -> true ; temIntersecao(T, Lista2)).
